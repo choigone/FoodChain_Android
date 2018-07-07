13db829b018dd74fe5c05ca99c802b140d4ab183
choigone <goun4439@gmail.com>
2018-07-08 08:29:56 +0900

    7/8 아침 8시반..졸려움.......

--- FoodChain2/app/src/main/java/com/team/foodchain/UserJoinActivity.kt
+++ FoodChain2/app/src/main/java/com/team/foodchain/UserJoinActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_join.*
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_master_join.*
 import kotlinx.android.synthetic.main.activity_user_join.*
 
 class UserJoinActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,10 +23,46 @@ class UserJoinActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    var pwText : EditText? =null
+    var pwCheckText : EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_join)
         user_join_btn.setOnClickListener(this)
+
+        pwText = findViewById(R.id.user_pw_et) as EditText
+        pwCheckText = findViewById(R.id.user_pwcheck_et) as EditText
+
+        pwText!!.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s1: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s1: CharSequence?, start: Int, before: Int, count: Int) {
+                pwCheckText!!.addTextChangedListener(object : TextWatcher {
+                    override fun afterTextChanged(s2: Editable?) {
+
+                    }
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    }
+
+                    override fun onTextChanged(s2: CharSequence?, start: Int, before: Int, count: Int) {
+                        if(s1!!.toString() == s2!!.toString()){
+                            user_pw_check.isSelected = true
+                        }
+                        else{
+                            user_pw_check.isSelected = false
+                        }
+                    }
+
+                })
+            }
+
+        })
 
 
     }
