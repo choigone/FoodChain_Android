9c88e1a74774bee8eaf5b37623d81be8e8a4f1e3
choigone <goun4439@gmail.com>
2018-07-13 21:59:06 +0900

    location 관련

--- FoodChain2/app/src/main/java/com/team/foodchain/Address2.kt
+++ FoodChain2/app/src/main/java/com/team/foodchain/Address2.kt
@@ -4,15 +4,23 @@ import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.frame_address2.*
 
-class Address2 : Fragment(){
 
+class Address2 : Fragment(), View.OnClickListener{
+    override fun onClick(v: View?) {
+            val index = address_list!!.getChildAdapterPosition(v!!)
+            Log.e("아이템클릭",addressItem[index])
+
+    }
+
+    lateinit var addressItem: ArrayList<String>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         lateinit var addressAdapter : AddressAdapter
-        lateinit var addressItem: ArrayList<String>
         val v = inflater!!.inflate(R.layout.frame_address2, container, false)
         lateinit var addressData : ArrayList<String>
         addressData = arguments!!.getStringArrayList("yes")
@@ -20,8 +28,8 @@ class Address2 : Fragment(){
         recyclerView.layoutManager = LinearLayoutManager(this.context)
         addressItem = addressData
         addressAdapter = AddressAdapter(addressItem)
+        addressAdapter.setOnItemClick(this)
         recyclerView.adapter = addressAdapter
-
         return v
     }
 }
